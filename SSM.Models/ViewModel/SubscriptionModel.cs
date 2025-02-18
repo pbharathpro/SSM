19ee117d3d56f1bc01a749afde0b12bff71732ca
pbharathpro <bharath.p@theproindia.com>
2025-02-18 22:10:21 +0530

    Validated fields in ViewModel

--- SSM.Models/ViewModel/SubscriptionModel.cs
+++ SSM.Models/ViewModel/SubscriptionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -9,9 +10,19 @@ namespace SSM.Models.ViewModel
 {
     public class SubscriptionModel
     {
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Name can only contain letters and spaces")]
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Category can only contain letters and spaces.")]
         public string Category { get; set; }
+
+        [Required]
+        [Range(1, 12, ErrorMessage = "Duration must be between 1 and 12 months.")]
         public int DurationMonths { get; set; }
+
+        [Required]
         public Guid UserId { get; set; }
 
 
--- SSM.Models/ViewModel/UserModel.cs
+++ SSM.Models/ViewModel/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace SSM.Models.ViewModel
 {
     public class UserModel
     {
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Name can only contain letters and spaces.")]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Password must contain at least one letter, one number, and one special character.")]
         public string Password { get; set; }
 
     }
